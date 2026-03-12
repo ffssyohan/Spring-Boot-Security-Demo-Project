60d0aab3759f44085b2e1e21fe52b84a186c6812
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-03-12 15:17:16 -0300

    Datasource and JdbcUserDetailsManager added to the project

--- src/main/java/com/example/securityDemo/GreetingsController.java
+++ src/main/java/com/example/securityDemo/GreetingsController.java
@@ -1,6 +1,5 @@
 package com.example.securityDemo;
 
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
